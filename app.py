e607be7545a99cb7d4f33aac62605db1070588df
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-02-08 21:44:12 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def info():
     url2 = 'https://test-api-6lv1.onrender.com/translateTest?text=buongiorno&sl=it&tl=de'
     url3 = 'https://test-api-6lv1.onrender.com/translateTest?text=hola&sl=sl&tl=fr'
     url4 = 'https://test-api-6lv1.onrender.com/translateTest?text=salut&sl=fr&tl=pt'
-    url5 = 'https://test-api-6lv1.onrender.com/translateTest?text=bonjour&sl=fr&tl=ru'
+    url5 = 'https://test-api-6lv1.onrender.com/translateTest?text=bonjour&sl=fr&tl=en'
 
     # Ritorna tutti gli esempi in stile HTML
     html += '<br><h1>Esempi Traduttore API</h1> '
@@ -49,10 +49,12 @@ def translate():
     sentences = re.split(r'[.!?]+', text)
     translated_sentences = []
     for sentence in sentences:
-        url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={source_language}&tl={target_language}&dt=t&q={sentence}"
-        response = requests.get(url)
-        data = response.json()
-        translated_sentences.append(data[0][0][0])
+        sentence = sentence.strip()
+        if sentence:  # verifica che la frase non sia vuota
+            url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={source_language}&tl={target_language}&dt=t&q={sentence}"
+            response = requests.get(url)
+            data = response.json()
+            translated_sentences.append(data[0][0][0])
     
     translated_text = " ".join(translated_sentences)
     
@@ -61,17 +63,19 @@ def translate():
 
 @app.route("/translateTest", methods=["GET"])
 def translateTest():
-    text = request.args.get("text")
-    source_language = request.args.get("sl")
-    target_language = request.args.get("tl")
+    text = request.json["text"]
+    source_language = request.json["sl"]
+    target_language = request.json["tl"]
     
     sentences = re.split(r'[.!?]+', text)
     translated_sentences = []
     for sentence in sentences:
-        url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={source_language}&tl={target_language}&dt=t&q={sentence}"
-        response = requests.get(url)
-        data = response.json()
-        translated_sentences.append(data[0][0][0])
+        sentence = sentence.strip()
+        if sentence:  # verifica che la frase non sia vuota
+            url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={source_language}&tl={target_language}&dt=t&q={sentence}"
+            response = requests.get(url)
+            data = response.json()
+            translated_sentences.append(data[0][0][0])
     
     translated_text = " ".join(translated_sentences)
     
