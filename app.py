7dccd731a687576783d9fbc30dd55099a67550fb
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-02-08 21:27:47 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def info():
-    return '''
+
+    html = '''
     <h1>Traduttore API</h1>
     <p>Questa è una semplice API di traduzione che utilizza Google Translate per tradurre testo da una lingua all'altra.</p>
     <p>Per utilizzare l'API, inviare una richiesta GET alla seguente URL:</p>
@@ -14,23 +15,21 @@ def info():
     <p>Sostituire "testo", "lingua_sorgente" e "lingua_destinazione" con i valori desiderati.</p>
     '''
 
-@app.route('/example')
-def example():
     # Esempi di richieste di traduzione
-    url1 = 'https://test-api-6lv1.onrender.com/translateTest?text=ciao&source_language=italiano&target_language=inglese'
-    url2 = 'https://test-api-6lv1.onrender.com/translateTest?text=buongiorno&source_language=italiano&target_language=tedesco'
-    url3 = 'https://test-api-6lv1.onrender.com/translateTest?text=hola&source_language=spagnolo&target_language=francese'
-    url4 = 'https://test-api-6lv1.onrender.com/translateTest?text=salut&source_language=francese&target_language=portoghese'
-    url5 = 'https://test-api-6lv1.onrender.com/translateTest?text=bonjour&source_language=francese&target_language=russo'
+    url1 = 'https://test-api-6lv1.onrender.com/translate?text=ciao&sl=italiano&tl=inglese'
+    url2 = 'https://test-api-6lv1.onrender.com/translate?text=buongiorno&sl=italiano&tl=tedesco'
+    url3 = 'https://test-api-6lv1.onrender.com/translate?text=hola&sl=spagnolo&tl=francese'
+    url4 = 'https://test-api-6lv1.onrender.com/translate?text=salut&sl=francese&tl=portoghese'
+    url5 = 'https://test-api-6lv1.onrender.com/translate?text=bonjour&sl=francese&tl=russo'
 
     # Ritorna tutti gli esempi in stile HTML
-    html = '<h1>Esempi Traduttore API</h1> '
+    html += '<br><h1>Esempi Traduttore API</h1> '
     html += '<ul>'
-    html += '<li>' + url1 + '</li>'
-    html += '<li>' + url2 + '</li>'
-    html += '<li>' + url3 + '</li>'
-    html += '<li>' + url4 + '</li>'
-    html += '<li>' + url5 + '</li>'
+    html += '<li>Traduci "ciao" dall\'italiano all\'inglese: ' + url1 + '</li>'
+    html += '<li>Traduci "buongiorno" dall\'italiano al tedesco: ' + url2 + '</li>'
+    html += '<li>Traduci "hola" dallo spagnolo al francese: ' + url3 + '</li>'
+    html += '<li>Traduci "salut" dal francese al portoghese: ' + url4 + '</li>'
+    html += '<li>Traduci "bonjour" dal francese al russo: ' + url5 + '</li>'
     html += '</ul>'
 
     return html
@@ -40,11 +39,12 @@ def example():
 def health_check():
     return "OK", 200
 
+
 @app.route("/translate", methods=["POST"])
 def translate():
     text = request.json["text"]
-    source_language = request.json["source_language"]
-    target_language = request.json["target_language"]
+    source_language = request.json["sl"]
+    target_language = request.json["tl"]
     
     sentences = re.split(r'[.!?]+', text)
     translated_sentences = []
@@ -58,11 +58,12 @@ def translate():
     
     return jsonify({"translated_text": translated_text})
 
+
 @app.route("/translateTest", methods=["GET"])
 def translateTest():
     text = request.args.get("text")
-    source_language = request.args.get("source_language")
-    target_language = request.args.get("target_language")
+    source_language = request.args.get("sl")
+    target_language = request.args.get("tl")
     
     sentences = re.split(r'[.!?]+', text)
     translated_sentences = []
